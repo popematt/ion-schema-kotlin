5a1adf4ece8447a591a92722e1c72d26d88d73e0
Matthew Pope <popematt@amazon.com>
2021-11-28 12:09:13 -0800

    Adds validate command to CLI

---
+++ cli/src/main/kotlin/com/amazon/ionschema/cli/commands/ValidateCommand.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazon.ionschema.cli.commands
+
+import com.amazon.ion.IonStruct
+import com.amazon.ion.IonText
+import com.amazon.ion.IonType
+import com.amazon.ion.system.IonSystemBuilder
+import com.amazon.ionschema.AuthorityFilesystem
+import com.amazon.ionschema.IonSchemaSystemBuilder
+import com.amazon.ionschema.ResourceAuthority
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.check
+import com.github.ajalt.clikt.parameters.groups.OptionGroup
+import com.github.ajalt.clikt.parameters.groups.defaultByName
+import com.github.ajalt.clikt.parameters.groups.groupChoice
+import com.github.ajalt.clikt.parameters.options.check
+import com.github.ajalt.clikt.parameters.options.multiple
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.file
+
+class ValidateCommand : CliktCommand(
+    help = "Validate Ion data against a schema.",
+    epilog = """
+        ```
+        Example usage:
+            Validate against a new type:
+                ion-schema-cli validate --type '{ codepoint_length: range::[min, 10] }' --value 'hello'
+            Validate against a type from a schema:
+                ion-schema-cli validate -a file-system --base-dir ~/my_schemas/ --schema 'Customers.isl' --type 'online_customer' --value '{ foo: bar }'    
+        ```
+    """.trimIndent()
+) {
+
+    private val ion = IonSystemBuilder.standard().build()
+
+    sealed class AuthorityConfig : OptionGroup() {
+        class FileSystem : AuthorityConfig() {
+            val baseDir by option().file().multiple(required = true)
+        }
+        class IonSchemaSchemas : AuthorityConfig()
+        object None : AuthorityConfig()
+    }
+
+    val authorityConfig by option("-a", "--authority").groupChoice(
+        "file-system" to AuthorityConfig.FileSystem(),
+        "isl" to AuthorityConfig.IonSchemaSchemas(),
+        "none" to AuthorityConfig.None
+    ).defaultByName("none")
+
+
+    val schema by option("-s", "--schema")
+
+    val type by option("-t", "--type", help = "An ISL type reference.").required().check {
+        with(ion.singleValue(it)) {
+            !isNullValue && type in listOf(IonType.SYMBOL, IonType.STRUCT, IonType.STRING)
+        }
+    }
+    val ionValue by argument("ion-value").check { runCatching { ion.singleValue(it) }.isSuccess }
+
+    override fun run() {
+        val authorities = when (authorityConfig) {
+            is AuthorityConfig.FileSystem -> (authorityConfig as AuthorityConfig.FileSystem).baseDir
+                .map { AuthorityFilesystem(it.absolutePath) }
+            is AuthorityConfig.IonSchemaSchemas -> listOf(ResourceAuthority.forIonSchemaSchemas())
+            is AuthorityConfig.None -> emptyList()
+        }
+
+        val iss = IonSchemaSystemBuilder.standard()
+            .withIonSystem(ion)
+            .withAuthorities(authorities)
+            .allowTransitiveImports(false)
+            .build()
+
+        val islSchema = when (authorityConfig) {
+            is AuthorityConfig.None -> iss.newSchema()
+            is AuthorityConfig.FileSystem -> iss.loadSchema(schema!!)
+            is AuthorityConfig.IonSchemaSchemas -> iss.loadSchema(schema!!)
+        }
+
+        val typeIon = iss.ionSystem.singleValue(type)
+        val islType = if (typeIon is IonText) {
+            islSchema.getType(typeIon.stringValue()) ?: throw IllegalArgumentException("No such type: $type")
+        } else {
+            islSchema.newType(typeIon as IonStruct)
+        }
+
+        val violations = islType.validate(ion.singleValue(ionValue))
+
+        if (violations.isValid()) {
+            echo("Valid")
+        } else {
+            echo(violations.toString().dropWhile { it != '\n' }.trim())
+        }
+    }
+}
